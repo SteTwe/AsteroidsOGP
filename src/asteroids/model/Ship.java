a2028b2a9d69c57c88cdcad24ac43e62d2cda3da
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-02-23 11:54:43 +0100

    added createShip

--- src/asteroids/model/Ship.java
+++ src/asteroids/model/Ship.java
@@ -1,11 +1,17 @@
 package asteroids.model;
-
+import asteroids.util.ModelException;
 /**
+ * A class of ships in a universe of Asteroids.	
  * 
  * @author joachim & stef
  * @version pre-alpha
+ * 
+ * 
  */
 
 public class Ship {
-
+	
+	public void createShip() throws ModelException{
+		
+	}
 }

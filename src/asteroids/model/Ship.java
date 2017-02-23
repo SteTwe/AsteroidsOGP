ae151282b97afc29cab29a03525bd940d7ba4e66
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-02-23 12:27:18 +0100

    added getShipRadius

--- src/asteroids/model/Ship.java
+++ src/asteroids/model/Ship.java
@@ -11,7 +11,24 @@ import asteroids.util.ModelException;
 
 public class Ship {
 	
+	
+	private static final double Radius = 0;
+
+
 	public void createShip() throws ModelException{
 		
 	}
+	
+	
+	// region getters
+	/**
+	 * 
+	 * @param ship
+	 * @return	the value of the Radius
+	 * @throws ModelException
+	 */
+	public double getShipRadius(Ship ship) throws ModelException{
+		return this.Radius;
+	}
+	
 }

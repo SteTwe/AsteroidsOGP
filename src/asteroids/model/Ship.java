c66b53d8ceb43ee37ac6313881f3fcc8d99affef
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-02-26 03:21:51 +0100

    restyled basic template, ready to code

--- src/asteroids/model/Ship.java
+++ src/asteroids/model/Ship.java
@@ -1,5 +1,6 @@
 package asteroids.model;
 import asteroids.util.ModelException;
+
 /**
  * A class of ships in a universe of Asteroids.	
  * 
@@ -10,25 +11,26 @@ import asteroids.util.ModelException;
  */
 
 public class Ship {
-	
-	
-	private static final double Radius = 0;
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param velocityX
+     * @param velocityY
+     * @param angle
+     * @param radius
+     */
+    public Ship(double x, double y,double velocityX, double velocityY,double angle, double radius){
+    }
+
+    ///Region regarding Position///
+
+    ///Region regarding Velocity///
+
+    ///Region regarding Orientation///
+
+    ///Region regarding Radius///
 
 
-	public void createShip() throws ModelException{
-		
-	}
-	
-	
-	// region getters
-	/**
-	 * 
-	 * @param ship
-	 * @return	the value of the Radius
-	 * @throws ModelException
-	 */
-	public double getShipRadius(Ship ship) throws ModelException{
-		return this.Radius;
-	}
-	
 }

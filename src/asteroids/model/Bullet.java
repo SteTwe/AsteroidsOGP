66d910991e9d93dd49a6374cd9a51f5f525643f0
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-04-08 13:17:39 +0200

    bullit init

--- src/asteroids/model/Bullet.java
+++ src/asteroids/model/Bullet.java
@@ -3,6 +3,21 @@ import be.kuleuven.cs.som.annotate.*;
 
 /**
  * Created by stef on 2-4-17.
+ * @author joachim
  */
 public class Bullet {
+
+    /**
+     * Initialize a bullet with x-position, y-position, velocity in x-direction, velocity in y-direction, radius.
+     *
+     * @param positionX The x-coordinate of this new bullet.
+     * @param positionY The y-coordinate of this new bullet.
+     * @param velocityX The velocity in the x-direction of this new bullet.
+     * @param velocityY The velocity in the y-direction of this new bullet.
+     * @param radius    The radius of this new bullet.
+     */
+    public Bullet(double positionX, double positionY, double velocityX, double velocityY, double radius) throws IllegalArgumentException {
+
+    }
+
 }

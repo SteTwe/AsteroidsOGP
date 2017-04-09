818bf21f105a7e25d2615199a6cfc23000d3b11e
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-04-09 20:15:18 +0200

    class entity following the ship class template

--- src/asteroids/model/Entity.java
+++ src/asteroids/model/Entity.java
@@ -1,8 +1,76 @@
 package asteroids.model;
 
+import be.kuleuven.cs.som.annotate.Basic;
+
 /**
  * Created by joachim on 09/04/2017.
- * @author joachim 
+ * @author joachim
  */
-public class Entity {
+public abstract class Entity {
+
+    /**
+     * Initialize a new entity with x-position, y-position, velocity in x-direction, velocity in y-direction, radius.
+     *
+     * @param positionX The x-coordinate of this new ship.
+     * @param positionY The y-coordinate of this new ship.
+     * @param velocityX The velocity in the x-direction of this new ship.
+     * @param velocityY The velocity in the y-direction of this new ship.
+     * @param radius    The radius of this new ship.
+     */
+    public Entity(double positionX, double positionY, double velocityX, double velocityY, double radius) throws IllegalArgumentException {
+        this.setPositionX(positionX);
+        this.setPositionY(positionY);
+        this.setVelocityX(velocityX);
+        this.setVelocityY(velocityY);
+        if (!isValidRadius(radius)) throw new IllegalArgumentException();
+        this.radius = radius;
+    }
+    
+    @Basic
+    public double getPositionX(){
+        return this.positionX;
+    }
+
+    private double positionX;
+
+    @Basic
+    public double getPositionY(){
+        return this.positionY;
+    }
+
+    private double positionY;
+
+    //TODO
+    private boolean isValidPositionX(double positionX){
+
+    }
+
+    //TODO
+    private void setPositionX(double positionX){
+
+    }
+
+    private boolean isValidPositionY(double positionY){
+
+    }
+    //TODO
+    private void setPositionY(double positionY){
+
+    }
+
+    @Basic
+    public double getVelocityX(){
+        return this.velocityX;
+    }
+
+    private double velocityX;
+
+    @Basic
+    public double getVelocityY(){
+        return this.velocityX;
+    }
+
+
+
 }
+

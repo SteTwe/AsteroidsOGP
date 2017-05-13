851061d2d212927495dd9cf77b6785437fe7c4e0
Stef Tweepenninckx <stef.tweepenninckx@student.kuleuven.be>
2017-05-13 17:07:17 +0200

    testing of world finished + renaming class

--- tests/asteroids/tests/testclass.java
+++ tests/asteroids/tests/testclass.java
@@ -15,7 +15,7 @@ import asteroids.part2.facade.IFacade;
 import asteroids.util.ModelException;
 
 
-public class testclassWorld {
+public class testclass {
     private static final double EPSILON = 0.0001;
 
     IFacade facade;
@@ -27,7 +27,7 @@ public class testclassWorld {
 
 
     @Test
-    public void testWorldEntities() throws ModelException{
+    public void testWorld() throws ModelException{
         World world = facade.createWorld(1000,800);
         //Test setters
         world.setWidth(1050);
@@ -42,17 +42,46 @@ public class testclassWorld {
         // Create some bullets
         Bullet bulletShip1 = facade.createBullet(0,0,15,2,2);
         Bullet bullet2 = facade.createBullet(15, 50, 2, 3, 9);
-        
-        // Add bullet2 to world, then load on ship and add ship to world
+
+        // Add bullet2 to world, then load bulletship1 on ship1 and add ship1 to world
         facade.addBulletToWorld(world, bullet2);
         facade.loadBulletOnShip(ship1, bulletShip1);
         facade.addShipToWorld(world, ship1);
-        // Expected : 1
-        System.out.println(facade.getWorldBullets(world));
-        
+        // Expected : 1, bullet on ship doesn't count as bullet on world
+        assertEquals(1,facade.getWorldBullets(world).size(), EPSILON);
+
+        // Add bullet2 to ship1
         facade.loadBulletOnShip(ship1, bullet2);
         // Expected : empty , bullet cannot be on ship and world at same time
         assertTrue(facade.getWorldBullets(world).isEmpty());
+
+        // Add ship2 & 3 to world
+        facade.addShipToWorld(world, ship2);
+        facade.addShipToWorld(world, ship3);
+        // Expected : 3, ship1, 2 & 3
+        assertEquals(3, facade.getWorldShips(world).size(), EPSILON);
+
+        Bullet bullet1 = facade.createBullet(600,150,2,4,6);
+        facade.addBulletToWorld(world, bullet1);
+        // Expected : 4, 3 ships + 1 bullet
+        assertEquals(4, facade.getEntities(world).size(), EPSILON);
+
+        // Expected : ship1
+        assertEquals(ship1, facade.getEntityAt(world, 250,350));
+
+        // Expected: bullet1
+        assertEquals(bullet1, facade.getEntityAt(world, 600,150));
+
+        // Expected: null
+        assertEquals(null, facade.getEntityAt(world, 15,956));
+
+        // Terminate world
+        facade.terminateWorld(world);
+        // Expected : all empty, termination == removal of entities
+        assertEquals(0, facade.getEntities(world).size(), EPSILON);
+        assertEquals(0, facade.getWorldShips(world).size(), EPSILON);
+        assertEquals(0,facade.getWorldBullets(world).size(), EPSILON);
+
 
     }
 }

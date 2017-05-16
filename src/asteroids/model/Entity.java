69eb78b0325dad7bdf766d304d8f7f9cdf4ac391
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-05-16 11:20:25 +0200

    getDistanceBetween doc fix

--- src/asteroids/model/Entity.java
+++ src/asteroids/model/Entity.java
@@ -473,10 +473,10 @@ public abstract class Entity {
 
     /**
      * Compute the distance between two entities. If the two compared entities are the same, distance is 0.
+     * @param        entity2
+     *              | Second entity
      * @return The distance between this entity and the given other entity.
-     * Calculated distance
-     * | return (Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2)));
-     * @param        entity2 | Second entity
+     *              | return (Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2)));
      */
     public double getDistanceBetween(Entity entity2) {
         double x1 = this.getPositionX();

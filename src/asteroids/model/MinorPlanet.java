89aebd2308a27a938c975116a31e6883891ae072
Stef Tweepenninckx <stef.tweepenninckx@student.kuleuven.be>
2017-05-18 16:34:09 +0200

    start MinorPlanet class

--- src/asteroids/model/MinorPlanet.java
+++ src/asteroids/model/MinorPlanet.java
@@ -1,7 +1,20 @@
 package asteroids.model;
 
+import be.kuleuven.cs.som.annotate.*;
+
 /**
  * Created by joachim on 18/05/2017.
  */
-public class MinorPlanet {
+public class MinorPlanet extends Entity{
+
+    public MinorPlanet(double positionX, double positionY, double velocityX, double velocityY, double radius){
+        super(positionX, positionY, velocityX, velocityY, radius);
+    }
+
+
+    
+    @Override
+    public void collide(Entity entity) {
+
+    }
 }

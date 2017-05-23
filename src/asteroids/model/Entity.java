8a40d5afcda64de02244db7b18089593afdc6afe
Joachim Opdenakker <joachim.opdenakker@outlook.com>
2017-05-23 15:44:26 +0200

    getTimeCollisionWithEntity new version

--- src/asteroids/model/Entity.java
+++ src/asteroids/model/Entity.java
@@ -564,23 +564,23 @@ public abstract class Entity implements Collideable{
 
         double[] velocityDifference = {ship2.getVelocityX() - this.getVelocityX(), ship2.getVelocityY() - this.getVelocityY()};
         double[] deltaV = velocityDifference;
-        
-        //TODO deltaV * delta R
-        double productVR = 0;
-        //TODO deltaV * deltaV
-        double productVV = 0;
-        //TODO deltaR * deltaR
-        double productRR = 0;
 
-        //TODO
-        double d = 0;
-        // if [deltaV * deltaR) >= 0
-        // time = Double.POSITIVE_INFINITY;
-        // if [(deltaV * deltaR)^2 - (deltaV * deltaR)*(deltaR * deltaR - radii^2)] <= 0
-        // time = Double.POSITIVE_INFINITY;
-        // deltaR = positionDifference
-        // deltaV = positionDifference
-        // -[(deltaV * deltaR) + sqrt(d)]/[deltaV * deltaV]
+        // deltaV * delta R
+        double productVR = positionDifference[0] * velocityDifference[0] + positionDifference[1] * velocityDifference[1];
+        // deltaV * deltaV
+        double productVV = Math.pow(velocityDifference[0], 2) + Math.pow(velocityDifference[1], 2);
+        // deltaR * deltaR
+        double productRR = Math.pow(positionDifference[0], 2) + Math.pow(positionDifference[1], 2);
+
+        // radius1 + radius2
+        double gamma = this.getRadius() + ship2.getRadius();
+
+        // (x1 - x2)^2 + (y1 - y2)^2
+        double gamma2 = (Math.pow((this.getPositionX() - ship2.getPositionX()), 2) +(Math.pow(this.getPositionY() - ship2.getPositionY(),2)));
+
+        // [(productVR)^2 - (productVR)*(productRR - gamma^2)]
+        double d = (Math.pow(productVR, 2)) - (productVR) * (productRR - gamma2);
+
         if (productVR >= 0)
             time = Double.POSITIVE_INFINITY;
         else if (d <= 0)
